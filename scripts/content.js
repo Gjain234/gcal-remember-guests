2e5ea923b93bb8a60771c79fe0d8c05539657c5d
Gauri Jain <gauri.g.jain@gmail.com>
2022-12-25 14:27:39 -0800

    simple chrome extensino to warn when you forgot to add guests to meeting

---
+++ scripts/content.js
@@ -0,0 +1,19 @@
+var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        // call events on key press
+        window.addEventListener("keydown", handleEvent, false);
+        function handleEvent(e) {
+            const people_invited_count = document.getElementById("xGstLst").children[0].childElementCount;
+            const title_value = document.getElementById("xTiIn").value;
+            const guestTab = document.getElementById("xGstTab");
+            if ((title_value.includes("<>") || title_value.includes("|") || title_value.includes("sync") || title_value.includes("meeting") || title_value.includes("chat")) && people_invited_count == 0) {
+                guestTab.style.backgroundColor = "red";
+            }
+            else {
+                guestTab.style.backgroundColor = "transparent";
+            }
+        };
+    }
+}, 10);
