449f36a7b6e18f15fa4ca3e4eb09293108519c15
Gauri Jain <gauri.g.jain@gmail.com>
2022-12-25 23:53:23 -0800

    add click listener

--- scripts/content.js
+++ scripts/content.js
@@ -4,9 +4,10 @@ var readyStateCheckInterval = setInterval(function () {
 
         // call events on key press
         window.addEventListener("keydown", handleEvent, false);
+        window.addEventListener("click", handleEvent, false);
         function handleEvent(e) {
             const people_invited_count = document.getElementById("xGstLst").children[0].childElementCount;
-            const title_value = document.getElementById("xTiIn").value;
+            const title_value = document.getElementById("xTiIn").value.toLowerCase();
             const guestTab = document.getElementById("xGstTab");
             if ((title_value.includes("<>") || title_value.includes("|") || title_value.includes("sync") || title_value.includes("meeting") || title_value.includes("chat")) && people_invited_count == 0) {
                 guestTab.style.backgroundColor = "red";
