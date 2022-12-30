2a88d6aa7f27e6a48553fa36766473168da06b31
Abhishek Sharma <abhishk@meta.com>
2022-12-30 14:27:03 +0530

    update logic for the dialog

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,53 @@
-var readyStateCheckInterval = setInterval(function () {
-    if (document.readyState === "complete") {
-        clearInterval(readyStateCheckInterval);
+const createBtn = document.querySelector('[aria-label="Create"]') //get the Create event button
+const meetingChecks = ['<>', '|', 'sync', 'chat', ':', 'meeting'] //identifiers of a meeting
 
-        // call events on key press
-        window.addEventListener("keydown", handleEvent, false);
-        window.addEventListener("click", handleEvent, false);
-        function handleEvent(e) {
-            const people_invited_count = document.getElementById("xGstLst").children[0].childElementCount;
-            const title_value = document.getElementById("xTiIn").value.toLowerCase();
-            const guestTab = document.getElementById("xGstTab");
-            if ((title_value.includes("<>") || title_value.includes("|") || title_value.includes("sync") || title_value.includes("meeting") || title_value.includes("chat")) && people_invited_count == 0) {
-                guestTab.style.backgroundColor = "red";
+//creating the error message
+const errorMsgContainer = document.createElement('div')
+errorMsgContainer.className = 'reminder-error'
+
+//text bit
+const errorMsg = document.createElement('span')
+errorMsg.innerText = 'Seems like you are creating a meeting with someone else, please remember to add other people too.'
+
+//icon
+const errorIcon = document.createElement('i')
+errorIcon.innerText = 'warning'
+errorIcon.className = 'google-material-icons meh4fc hggPq GyffFb'
+
+//appending the icon and text to the main error container
+errorMsgContainer.appendChild(errorIcon)
+errorMsgContainer.appendChild(errorMsg)
+
+//hiding the error to start with
+errorMsgContainer.style.display = 'none'
+errorMsgContainer.style.opacity = 0
+
+//every time, the button is clicked – start the timer and wait for the pop-up dialog to open
+createBtn.onclick = function () {
+
+    let readyStateCheckInterval = setInterval(function () {
+        const eventNameInput = document.querySelector('[aria-label="Add title"]')
+        //check if the pop-up dialog has been created
+        if (eventNameInput != null) {
+
+            document.querySelector('[role="tabpanel"]').parentElement.appendChild(errorMsgContainer)
+            //listen for inpiut
+            let isMeetingWithSomeone
+            eventNameInput.oninput = () => {
+                //check if the meeting name has any of the signals that it is not a single person meeting
+                isMeetingWithSomeone = meetingChecks.some(identifier => eventNameInput.value.toLowerCase().includes(identifier));
+
+                //depending on our guess, show/hide the error
+                if (isMeetingWithSomeone) {
+                    errorMsgContainer.style.opacity = 1
+                    errorMsgContainer.style.display = 'inline-flex'
+                } else {
+                    errorMsgContainer.style.opacity = 0
+                    errorMsgContainer.style.display = 'none'
+                }
             }
-            else {
-                guestTab.style.backgroundColor = "transparent";
-            }
-        };
-    }
-}, 10);
+            //stop the timer
+            clearInterval(readyStateCheckInterval);
+        }
+    }, 10);
+}
