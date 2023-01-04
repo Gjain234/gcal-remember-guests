4cc9c9f489c05607e6bfad7e216e5f297a858065
Gauri Jain <gauri.g.jain@gmail.com>
2023-01-04 15:47:27 -0800

    add dialog functionality when clicking calendar

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,24 @@
 // keep checking URL
 var oldURL = ""
+var currInterval = null
 function checkUrlChange() {
     newURL = document.URL;
     if (newURL !== oldURL && newURL != null) {
         if (newURL.includes("eventedit")) {
             errorMsgContainer.classList.add('reminder-error-create-page')
-            eventEditPage();
+            clearInterval(currInterval)
+            currInterval = eventEditPage();
         }
         else {
             errorMsgContainer.classList.remove('reminder-error-create-page')
-            eventEditDialog();
+            clearInterval(currInterval)
+            currInterval = eventEditDialog();
         }
         oldURL = newURL;
     }
 };
+
+var urlChangeHandler = window.setInterval(checkUrlChange, 10);
 
 const createBtn = document.querySelector('[aria-label="Create"]') //get the Create event button
 const meetingChecks = ['<>', '|', 'sync', 'chat', ':', 'meeting'] //identifiers of a meeting
@@ -37,18 +42,15 @@ errorMsgContainer.appendChild(errorMsg)
 errorMsgContainer.style.display = 'none'
 errorMsgContainer.style.opacity = 0
 
-var urlChangeHandler = window.setInterval(checkUrlChange, 10);
-
 // js for event edit page vs dialog
 
 function eventEditPage() {
-    let readyStateCheckInterval = setInterval(function () {
+    return setInterval(function () {
         guest_input = document.querySelector('[aria-label="Guests"]');
         guest_section = getParentNode(guest_input, 5);
         guest_list = document.getElementById("xGstLst")?.children[0];
         title_element = document.getElementById("xTiIn");
         if (document.readyState === "complete" && guest_section != null && title_element != null && guest_input != null && guest_list != null) {
-            clearInterval(readyStateCheckInterval);
             guest_section.classList.add("guest-section-override");
             guest_section.appendChild(errorMsgContainer);
             function handleEventPageEdit() {
@@ -73,43 +75,39 @@ function eventEditPage() {
     }, 10);
 }
 function eventEditDialog() {
-    createBtn.onclick = function () {
-        let readyStateCheckInterval = setInterval(function () {
-            const eventNameInput = document.querySelector('[aria-label="Add title"]')
-            guest_input = document.querySelector('[aria-label="Guests"]');
-            event_list = document.querySelector('[aria-label="Guests invited to this event."]');
-            //check if the pop-up dialog has been created
-            if (eventNameInput != null && event_list != null) {
-                document.querySelector('[role="tabpanel"]').parentElement.appendChild(errorMsgContainer)
-                //listen for inpiut
-                let isMeetingWithSomeone
-                function handleEventDialogEdit() {
-                    //check if the meeting name has any of the signals that it is not a single person meeting
-                    isMeetingWithSomeone = meetingChecks.some(identifier => eventNameInput.value.toLowerCase().includes(identifier));
-                    people_invited_count = event_list?.childElementCount;
-                    //depending on our guess, show/hide the error
-                    if (isMeetingWithSomeone && people_invited_count != null && people_invited_count == 0) {
-                        errorMsgContainer.style.opacity = 1
-                        errorMsgContainer.style.display = 'inline-flex'
-                    } else {
-                        errorMsgContainer.style.opacity = 0
-                        errorMsgContainer.style.display = 'none'
-                    }
+    return setInterval(function () {
+        const eventNameInput = document.querySelector('[aria-label="Add title"]');
+        guest_input = document.querySelector('[aria-label="Guests"]');
+        guest_list = document.querySelector('[aria-label="Guests invited to this event."]');
+        //check if the pop-up dialog has been created
+        if (eventNameInput != null && guest_list != null && eventNameInput.value!= '') {
+            document.querySelector('[role="tabpanel"]').parentElement.appendChild(errorMsgContainer)
+            //listen for inpiut
+            let isMeetingWithSomeone
+            function handleEventDialogEdit() {
+                //check if the meeting name has any of the signals that it is not a single person meeting
+                isMeetingWithSomeone = meetingChecks.some(identifier => eventNameInput.value.toLowerCase().includes(identifier));
+                people_invited_count = guest_list?.childElementCount;
+                //depending on our guess, show/hide the error
+                if (isMeetingWithSomeone && people_invited_count != null && people_invited_count == 0) {
+                    errorMsgContainer.style.opacity = 1
+                    errorMsgContainer.style.display = 'inline-flex'
+                } else {
+                    errorMsgContainer.style.opacity = 0
+                    errorMsgContainer.style.display = 'none'
                 }
-                function addGuestsDialog() {
-                    setTimeout(handleEventDialogEdit, 2000);
-                }
-                eventNameInput.oninput = handleEventDialogEdit;
-                window.addEventListener("click", addGuestsDialog, false);
-                //stop the timer
-                clearInterval(readyStateCheckInterval);
-
-                //also hide the error once the dialog is closed
-                errorMsgContainer.style.display = 'none'
-                errorMsgContainer.style.opacity = 0
             }
-        }, 10);
-    }
+            function addGuestsDialog() {
+                setTimeout(handleEventDialogEdit, 2000);
+            }
+            eventNameInput.oninput = handleEventDialogEdit;
+            window.addEventListener("click", addGuestsDialog, false);
+        }else{
+            isMeetingWithSomeone = false;
+            errorMsgContainer.style.opacity = 0
+            errorMsgContainer.style.display = 'none'
+        }
+    }, 10);
 }
 
 // helper functions
